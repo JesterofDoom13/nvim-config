6268e0a7a01b21f5a056600d857c81b2fedfc660
Nicholas Porter <JesterofDoom13@gmail.com>
2026-04-11 11:33:45 -0600

    auto: pre-rebuild snapshot

--- lua/plugins/fastaction.lua
+++ lua/plugins/fastaction.lua
@@ -1,27 +1,36 @@
 return {
-	dismiss_keys = { "j", "k", "<c-c>", "q" },
-	override_function = function(_) end,
-	keys = "qwertyuiopasdfghlzxcvbnm",
-	popup = {
-		border = "rounded",
-		hide_cursor = true,
-		highlight = {
-			action = "MoreMsg",
-			divider = "FloatBorder",
-			key = "MoreMsg",
-			source = "Comment",
-			title = "Title",
-			window = "NormalFloat",
+	"Chaitanyabsprip/fastaction.nvim",
+	-- @type FastActionConfig
+	opts = {
+		dismiss_keys = { "j", "k", "<c-c>", "q" },
+		override_function = function(params) -- to retain built-in style keymaps
+			params.invalid_keys[#params.invalid_keys + 1] = tostring(#params.invalid_keys + 1)
+			return { key = tostring(#params.invalid_keys), order = 0 }
+		end,
+		keys = "asdfghlzxcvbnm",
+		popup = {
+			border = "rounded",
+			hide_cursor = true,
+			highlight = {
+				action = "MoreMsg",
+				divider = "FloatBorder",
+				key = "MoreMsg",
+				source = "Comment",
+				title = "Title",
+				window = "NormalFloat",
+			},
+			title = "Select one of:", -- or false to disable title
 		},
-		title = "Select one of:",
+		priority = {
+			dart = {
+				{ pattern = "organize import", key = "o", order = 1 },
+				{ pattern = "extract method", key = "x", order = 2 },
+				{ pattern = "extract widget", key = "e", order = 3 },
+			},
+			typescript = {
+				{ pattern = "to existing import declaration", key = "a", order = 2 },
+				{ pattern = "from module", key = "i", order = 1 },
+			},
+		},
 	},
-	priority = {
-		-- dart = {
-		--   { pattern = "organize import", key ="o", order = 1 },
-		--   { pattern = "extract method", key ="x", order = 2 },
-		--   { pattern = "extract widget", key ="e", order = 3 },
-		-- },
-	},
-	register_ui_select = false,
-	format_right_section = nil,
 }
