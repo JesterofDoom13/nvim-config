f5047589687450b182eb5d1e3aa3ec9d907afb31
Nicholas Porter <JesterofDoom13@gmail.com>
2026-04-12 22:21:07 -0600

    auto: pre-rebuild snapshot

--- lua/noctalia-colors.lua
+++ lua/noctalia-colors.lua
@@ -2,55 +2,55 @@ local M = {}
 
 function M.setup()
 	local colors = {
-		base00 = "#13122c",
-		base01 = "#201d49",
-		base02 = "#1d1a42",
-		base03 = "#626175",
+		base00 = "#292514",
+		base01 = "#453e21",
+		base02 = "#3e381e",
+		base03 = "#726e5e",
 
-		base04 = "#afafb6",
-		base05 = "#f2f2f3",
-		base06 = "#f2f2f3",
-		base07 = "#f2f2f3",
+		base04 = "#b6b5af",
+		base05 = "#f3f3f2",
+		base06 = "#f3f3f2",
+		base07 = "#f3f3f2",
 
 		base08 = "#fd4663",
-		base09 = "#cc66c5",
-		base0A = "#7067e4",
-		base0B = "#a25cd6",
-		base0C = "#150c8d",
-		base0D = "#7067e4",
-		base0E = "#691664",
+		base09 = "#b4f7a4",
+		base0A = "#f7e7a4",
+		base0B = "#def7a4",
+		base0C = "#e7bb01",
+		base0D = "#f7e7a4",
+		base0E = "#2ee701",
 		base0F = "#900017",
 	}
 	local lualinecolors = {
 		normal = {
-			a = { fg = "#191825", bg = "#7067e4", gui = "bold" },
-			b = { fg = "#f2f2f3", bg = "#1a173a" },
-			c = { fg = "#626175", bg = "#13122c" },
+			a = { fg = "#252218", bg = "#f7e7a4", gui = "bold" },
+			b = { fg = "#f3f3f2", bg = "#37321b" },
+			c = { fg = "#726e5e", bg = "#292514" },
 		},
 		insert = {
-			a = { fg = "#191825", bg = "#cc66c5", gui = "bold" },
-			b = { fg = "#f2f2f3", bg = "#1a173a" },
+			a = { fg = "#252218", bg = "#b4f7a4", gui = "bold" },
+			b = { fg = "#f3f3f2", bg = "#37321b" },
 		},
 		visual = {
-			a = { fg = "#191825", bg = "#a25cd6", gui = "bold" },
-			b = { fg = "#f2f2f3", bg = "#1a173a" },
+			a = { fg = "#252218", bg = "#def7a4", gui = "bold" },
+			b = { fg = "#f3f3f2", bg = "#37321b" },
 		},
 		replace = {
-			a = { fg = "#191825", bg = "#fd4663", gui = "bold" },
-			b = { fg = "#f2f2f3", bg = "#1a173a" },
+			a = { fg = "#252218", bg = "#fd4663", gui = "bold" },
+			b = { fg = "#f3f3f2", bg = "#37321b" },
 		},
 		command = {
 			a = {
-				fg = "#e7dbf0",
-				bg = "#47106f",
+				fg = "#eefbd0",
+				bg = "#a2e701",
 				gui = "bold",
 			},
-			b = { fg = "#f2f2f3", bg = "#1a173a" },
+			b = { fg = "#f3f3f2", bg = "#37321b" },
 		},
 		inactive = {
-			a = { fg = "#626175", bg = "#13122c" },
-			b = { fg = "#626175", bg = "#13122c" },
-			c = { fg = "#626175", bg = "#13122c" },
+			a = { fg = "#726e5e", bg = "#292514" },
+			b = { fg = "#726e5e", bg = "#292514" },
+			c = { fg = "#726e5e", bg = "#292514" },
 		},
 	}
 	local status, base16 = pcall(require, "base16-colorscheme")
