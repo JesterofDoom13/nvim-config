326d926eacaf9e17d99c14d1d7b79d8aa68a1658
Nicholas Porter <JesterofDoom13@gmail.com>
2026-09-04 07:58:23 -0600

    Fix: rustaceanvim
    Fix: rustaceanvim would throw an error when not in a rust environment
    which is the typical place. Now has a conditional statement if cargo
    runs before it loads the plugin

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -1,11 +1,27 @@
-vim.g.rustaceanvim = {
-	server = {
-		default_settings = {
-			["rust-analyzer"] = {
-				check = {
-					allTargets = false,
+return {
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^5", -- Recommended version constraint
+		lazy = false, -- This plugin handles its own lazy-loading
+		cond = function()
+			return vim.fn.executable("cargo --version") == 1
+		end,
+		config = function(_, opts)
+			vim.g.rustaceanvim = {
+				-- Configure server settings here
+				server = {
+					-- on_attach = function(client, bufnr)
+					--   -- Custom keymaps or attach logic can go here
+					-- end,
+					settings = {
+						["rust-analyzer"] = {
+							cargo = {
+								allFeatures = true,
+							},
+						},
+					},
 				},
-			},
-		},
+			}
+		end,
 	},
 }
